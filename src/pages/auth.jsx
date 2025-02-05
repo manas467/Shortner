17d09663869bc7cb3d5950c208d79deecae69ce4
manas467 <137690405+manas467@users.noreply.github.com>
2025-02-05 13:19:12 +0530

    fixed login

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,4 +1,4 @@
-import Login from "@/components/login";
+import Login from "@/components/Login";
 import Signup from "@/components/Signup";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {UrlState} from "@/context";
